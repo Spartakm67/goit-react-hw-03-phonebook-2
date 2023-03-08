bbc3e8fc5d5e95b0ba3c9155549a1427bd83d3cc
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-03-08 13:11:35 +0200

    Initial commit

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+    padding: 50px;   
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    color: #8b00ff;
+   `;
+
+export const IfEmpty = styled.p`
+   color: green;
+   font-size: 25px;
+   font-weight: 500;
+`;
